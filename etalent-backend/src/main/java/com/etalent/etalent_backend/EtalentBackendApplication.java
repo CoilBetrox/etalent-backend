46836e51441917dd7c5da670852a20b5c969c415
roberthdanielt <roberthdanielt@gmail.com>
2024-05-30 12:21:06 -0500

    first update

---
+++ etalent-backend/src/main/java/com/etalent/etalent_backend/EtalentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.etalent.etalent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtalentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtalentBackendApplication.class, args);
+	}
+
+}
---
+++ etalent-backend/src/test/java/com/etalent/etalent_backend/EtalentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etalent.etalent_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtalentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
